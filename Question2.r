13cfebd443063eab0a4035bfa148d263cd73dac5
t-chu4 <t-chu4@cs.sfu.ca>
2025-01-27 21:39:03 -0800

    whitespace

--- Question2.r
+++ Question2.r
@@ -9,24 +9,23 @@ library(ggplot2) # Missed the tutorial for this, whoops
 
 # Basic table read
 data <- read.csv("Group_Assignment_Dataset.txt",
-                 sep = ",",
-                 header = T,)
+                  sep = ",",
+                  header = T,)
 
 # Columns, minus date/time
 data_vars <- c("Global_active_power",
-                   "Global_reactive_power", 
-                   "Voltage",
-                   "Global_intensity", 
-                   "Sub_metering_1",
-                   "Sub_metering_2",
-                   "Sub_metering_3")
+               "Global_reactive_power", 
+               "Voltage",
+               "Global_intensity", 
+               "Sub_metering_1",
+               "Sub_metering_2",
+               "Sub_metering_3")
 
 # Precautionary, remove empty things
 data <- na.omit(data)
 
 # Matrix calculated with command taken directly from assignment info
-matrix <- cor(data[data_vars],
-                  method = "pearson")
+matrix <- cor(data[data_vars], method = "pearson")
 
 # Assume math is correct as calculation shown in class. Display appropriate...?
 print(matrix)
